1c4a58e5bb855789707acb0b1ade52a6472cc5bd
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-02 21:09:58 +0530

    feat: complete the auth.schema.ts

---
+++ src/modules/auth/auth.repository.ts
@@ -0,0 +1,5 @@
+export const authRepository = {
+    findByEmail(email: string) {
+        
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config()
+import app from "./app";
+
+app.listen(5000, () => console.log(`server is running at 5000`))
