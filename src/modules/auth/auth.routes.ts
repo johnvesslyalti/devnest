9b9517fd6327fb2e399df2f2141290a16d8e15e5
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-05 15:53:40 +0530

    fix: auth bug in the auth middleware

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { loginSchema, registerSchema } from "./auth.schema";
 import { validate } from "../../middlewares/validate";
 import { authController } from "./auth.controller";
+import { auth } from "../../middlewares/auth";
 
 const router = Router()
 
 router.post("/register", validate(registerSchema), authController.register)
 router.post("/login", validate(loginSchema), authController.login)
-router.delete("/", validate(loginSchema), authController.delete)
+router.delete("/", auth.verifyToken, authController.delete)
 
 export default router;
