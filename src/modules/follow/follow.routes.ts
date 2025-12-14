ceb3ba519859a26268de03b0479a14ee9823b354
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-14 13:16:46 +0530

    feat: complete the follow

--- src/modules/follow/follow.routes.ts
+++ src/modules/follow/follow.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { auth } from "../../middlewares/auth";
 import { followController } from "./follow.controller";
 
 const router = Router();
 
-router.post("/", auth.verifyAccessToken, followController.follow);
-router.delete("/", auth.verifyAccessToken, followController.unfollow);
+router.post("/", followController.follow);
+router.delete("/", followController.unfollow);
 
 export default router;
